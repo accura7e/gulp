7242bfdaae9f07af7780676ed462dfbfa96bd437
accura7e <accura7ee@gmail.com>
2023-08-28 18:57:03 +0300

    add: added group-media

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,19 @@ const gulp = require('gulp');
 const fileInclude = require('gulp-file-include');
 const sass = require('gulp-sass')(require('sass'));
 const server = require('gulp-server-livereload');
+const clean = require('gulp-clean');
+const fs = require('fs');
+const sourceMaps = require('gulp-sourcemaps');
+const media = require('gulp-group-css-media-queries')
+
+gulp.task('clean', function(done){
+    if (fs.existsSync('./dist/')){
+        return gulp.src('./dist/', {read: false}).pipe(clean())
+    }
+
+    done();
+})
+
 
 const fileIncludeSetting = {
     prefix: '@@',
@@ -17,7 +30,7 @@ const serverOptions = {
     
 
 
-gulp.task('includeFiles', function(){
+gulp.task('html', function(){
     return gulp
         .src('./src/*.html')
         .pipe(fileInclude(fileIncludeSetting))
@@ -26,15 +39,31 @@ gulp.task('includeFiles', function(){
 
 gulp.task('sass', function(){
     return gulp.src('./src/scss/*.scss')
+    .pipe(sourceMaps.init())
     .pipe(sass())
+    .pipe(groupMedia())
+    .pipe(sourceMaps.write())
     .pipe(gulp.dest('./dist/css/'))
 })
 
-gulp.task('copyImages', function(){
+gulp.task('images', function(){
     return gulp.src('./src/images/**/*')
     .pipe(gulp.dest('./dist/img'))
 })
 
-gulp.task('startServer', function(){
+gulp.task('server', function(){
     return gulp.src('./dist/').pipe(server(serverOptions))
 })
+
+gulp.task('watch', function(){
+    gulp.watch('./src/scss/**/*.scss', gulp.parallel('sass'))
+    gulp.watch('./src/**/*.html', gulp.parallel('html'));
+    gulp.watch('./src/images/**/*', gulp.parallel('images'));
+})
+
+
+gulp.task('default', gulp.series(
+    'clean', 
+    gulp.parallel('html', 'sass', 'images'),
+    gulp.parallel('server', 'watch')
+    ))
